c8bd53d7f3d22958e9df83df17281394bc7b916e
gabrielvicto0802 <gabrirlvictor0802@gmail.com>
2021-05-31 09:46:53 -0300

    subindo array

---
+++ index.js
@@ -0,0 +1,19 @@
+let lista = ["gabriel","luan","tati","joão","francisco"]
+
+lista.unshift("Marina")
+
+lista.pop()
+
+lista.push("Dennis", "Edmundo")
+
+lista.shift()
+
+console.log(lista)
+
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+
+console.log(numbers)
+
